fd924f5874511747986f035e979a59976dccbddd
Muhsin ARSLAN <marstan.gelistirici@outlook.com>
2021-02-20 18:28:31 +0300

    Update

---
+++ 16-cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <TheHeader />
+    <BadgeList />
+    <UserInfo
+      :fullName="activeUser.name"
+      :infoText="activeUser.description"
+      :role="activeUser.role"
+    />
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+
+export default {
+  components: { TheHeader, BadgeList, UserInfo },
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
